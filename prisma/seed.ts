b21b89f8ae4ab774ff1995f7cf6c05524dc8d52f
rodrigoronin <liramailbox@gmail.com>
2023-01-23 18:41:39 -0300

    created rout to return habits

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,36 +2,98 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const habitsListMock = [
-  {
-    title: "Beber água 2.5L",
-    created_at: new Date("2023-01-23T00:00:00.000z"),
-  },
-  {
-    title: "Treinar",
-    created_at: new Date("2023-01-23T00:00:00.000z"),
-  },
-  {
-    title: "Estudar JavaScript",
-    created_at: new Date("2023-01-23T00:00:00.000z"),
-  },
-];
+const firstHabitId = "0730ffac-d039-4194-9571-01aa2aa0efbd";
+const firstHabitCreationDate = new Date("2022-12-31T03:00:00.000");
 
-async function mains() {
+const secondHabitId = "00880d75-a933-4fef-94ab-e05744435297";
+const secondHabitCreationDate = new Date("2023-01-21T03:00:00.000");
+
+const thirdHabitId = "fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00";
+const thirdHabitCreationDate = new Date("2023-01-23T03:00:00.000");
+
+async function run() {
+  await prisma.habitWeekDays.deleteMany();
+  await prisma.day.deleteMany();
   await prisma.habit.deleteMany();
 
-  // prisma.createMany() doesn't work in SQLite, so I need to use a loop to create more than one entry
-  for (const habits of habitsListMock) {
-    await prisma.habit.create({
+  // Create habits
+  await Promise.all([
+    prisma.habit.create({
       data: {
-        title: habits.title,
-        created_at: habits.created_at,
+        id: firstHabitId,
+        title: "Beber 2L de água",
+        created_at: firstHabitCreationDate,
+        WeekDays: {
+          create: [{ week_day: 1 }, { week_day: 2 }, { week_day: 3 }],
+        },
       },
-    });
-  }
+    }),
+    prisma.habit.create({
+      data: {
+        id: secondHabitId,
+        title: "Treinar",
+        created_at: secondHabitCreationDate,
+        WeekDays: {
+          create: [{ week_day: 3 }, { week_day: 4 }, { week_day: 5 }],
+        },
+      },
+    }),
+    prisma.habit.create({
+      data: {
+        id: thirdHabitId,
+        title: "Dormir 8h/dia",
+        created_at: thirdHabitCreationDate,
+        WeekDays: {
+          create: [
+            { week_day: 1 },
+            { week_day: 2 },
+            { week_day: 3 },
+            { week_day: 4 },
+            { week_day: 5 },
+          ],
+        },
+      },
+    }),
+  ]);
+
+  await Promise.all([
+    /**
+     * Habits (Complete/Available): 1/1
+     */
+    prisma.day.create({
+      data: {
+        date: new Date("2023-01-02T03:00:00.000z"),
+        DayHabit: {
+          create: {
+            habit_id: firstHabitId,
+          },
+        },
+      },
+    }),
+
+    prisma.day.create({
+      data: {
+        date: new Date("2023-01-06T03:00:00.000z"),
+        DayHabit: {
+          create: {
+            habit_id: secondHabitId,
+          },
+        },
+      },
+    }),
+
+    prisma.day.create({
+      data: {
+        date: new Date("2023-01-04T03:00:00.000z"),
+        DayHabit: {
+          create: [{ habit_id: firstHabitId }, { habit_id: secondHabitId }],
+        },
+      },
+    }),
+  ]);
 }
 
-mains()
+run()
   .then(async () => {
     await prisma.$disconnect();
   })
